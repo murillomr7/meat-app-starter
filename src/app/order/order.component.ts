d836af015bc0307329f44f61281b07b9fd850dc6
Murillo M Raggiotto <murillomr7@MacBook-Pro-de-Murillo.local>
2017-11-11 23:40:56 -0300

    add radio component

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from '../shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order-container',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  paymentOptions: RadioOption[] = [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartão de débito', value: 'DEB'},
+    {label: 'Cartão refeição', value: 'REF'},
+    
 
+  ]
+
+  constructor() { }
+ 
   ngOnInit() {
   }
 
